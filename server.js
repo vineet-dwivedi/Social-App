69f2eb0d4081208ccbdf606214eb639ab3489b91
Vineet Dwivedi <vineetdwivedi409@gmail.com>
2026-02-20 16:40:43 +0530

    Delete server.js

--- server.js
+++
@@ -1,9 +0,0 @@
-require('dotenv').config();
-const app = require('./src/app');
-const connect = require('./src/config/db')
-const PORT = 5000;
-
-connect();
-app.listen(PORT , ()=>{
-    console.log(`Server is running on ${PORT}`)
-})
