080d7c65b96fae25bdb9bc7b2f2de35c3bb6148c
Vineet Dwivedi <vineetdwivedi409@gmail.com>
2026-02-20 16:39:25 +0530

    Delete src directory

--- src/config/db.js
+++
@@ -1,6 +0,0 @@
-const mongo = require('mongoose');
- async function connect(){
-    await mongo.connect(process.env.MONGO_URI)
-    console.log('Connected To MONGO')
-}
-module.exports = connect;
--- src/routes/auth.routes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const authRouter = express.Router();
-const authController = require('../controllers/auth.controller')
-
-authRouter.post('/register', authController.registerController)
-
-authRouter.post('/login', authController.loginController)
-module.exports = authRouter;
